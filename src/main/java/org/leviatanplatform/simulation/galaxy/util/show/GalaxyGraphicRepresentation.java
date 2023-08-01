231c8a34afe123b1ff7327846065b64c375cb4c3
CiudadAlex <ciudad.alex@gmail.com>
2023-08-02 01:10:45 +0200

    initial commit

--- src/main/java/org/leviatanplatform/simulation/galaxy/util/show/GalaxyGraphicRepresentation.java
+++ src/main/java/org/leviatanplatform/simulation/galaxy/util/show/GalaxyGraphicRepresentation.java
@@ -3,23 +3,46 @@ package org.leviatanplatform.simulation.galaxy.util.show;
 import org.leviatanplatform.simulation.galaxy.engine.model.Galaxy;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GalaxyGraphicRepresentation {
 
     private JFrame frame;
+    private JPanel canvas;
+    private Galaxy galaxy;
 
     public void show(Galaxy galaxy) {
 
-        if (frame != null) {
-            frame.dispose();
+        this.galaxy = galaxy;
+
+        if (frame == null) {
+            frame = new JFrame("GALAXY");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1600, 900);
+            frame.setVisible(true);
+            frame.setResizable(false);
+
+            canvas = createCanvas();
+            frame.add(canvas);
         }
 
-        frame = new JFrame("GALAXY");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(GalaxyGraphicRepresentationUtils.createCanvas(galaxy));
-        frame.setSize(1600, 900);
-        frame.setVisible(true);
-        frame.setResizable(false);
+        SwingUtilities.invokeLater(() -> {
+            canvas.invalidate();
+            canvas.validate();
+            canvas.repaint();
+        });
+    }
+
+    private JPanel createCanvas() {
+
+        return new JPanel() {
+
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                GalaxyGraphicRepresentationUtils.paintCurrentListOfStars(g, galaxy.listStar(), getWidth(), getHeight());
+            }
+        };
     }
 
 }
--- src/main/java/org/leviatanplatform/simulation/galaxy/util/show/GalaxyGraphicRepresentationUtils.java
+++ src/main/java/org/leviatanplatform/simulation/galaxy/util/show/GalaxyGraphicRepresentationUtils.java
@@ -1,38 +1,24 @@
 package org.leviatanplatform.simulation.galaxy.util.show;
 
-import org.leviatanplatform.simulation.galaxy.engine.model.Galaxy;
 import org.leviatanplatform.simulation.galaxy.engine.model.Star;
 import org.leviatanplatform.simulation.galaxy.engine.model.Vector;
 import org.leviatanplatform.simulation.galaxy.util.RandomStarGenerator;
 
-import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
 public class GalaxyGraphicRepresentationUtils {
 
-    // FIXME test
-
-    public static JPanel createCanvas(Galaxy galaxy) {
-
-        JPanel canvas = new JPanel() {
-            @Override
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                int pixelsReference = Math.min(getWidth(), getHeight());
-                GalaxyGraphicRepresentationUtils.paintCurrentListOfStars(g, galaxy.listStar(), pixelsReference);
-            }
-        };
-
-        return canvas;
-    }
-
-    public static void paintCurrentListOfStars(Graphics g, List<Star> listOfStar, int pixelsReference) {
+    public static void paintCurrentListOfStars(Graphics g, List<Star> listOfStar, int width, int height) {
 
         double maxDistantStar = listOfStar.stream().map(star -> star.position().norm()).mapToDouble(d -> d).max()
                 .orElse(RandomStarGenerator.MILKY_WAY_RADIUS);
 
         g.setColor(Color.black);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.white);
+
+        int pixelsReference = Math.min(width, height);
 
         for (Star star : listOfStar) {
             paintStar(g, star, maxDistantStar, pixelsReference);
@@ -43,6 +29,6 @@ public class GalaxyGraphicRepresentationUtils {
         Vector position = star.position();
         int x = (int) (pixelsReference * position.x() / maxDistantStar);
         int y = (int) (pixelsReference * position.y() / maxDistantStar);
-        g.drawRect(x, y, 1, 1);
+        g.fillOval(x, y, 1, 1);
     }
 }
