eafb1b3d9feb397614b6cdab406e8a5a03adb237
CiudadAlex <ciudad.alex@gmail.com>
2023-08-02 14:11:56 +0200

    initial commit

--- src/main/java/org/leviatanplatform/simulation/galaxy/util/show/GalaxyGraphicRepresentationUtils.java
+++ src/main/java/org/leviatanplatform/simulation/galaxy/util/show/GalaxyGraphicRepresentationUtils.java
@@ -13,7 +13,6 @@ public class GalaxyGraphicRepresentationUtils {
 
         g.setColor(Color.black);
         g.fillRect(0, 0, width, height);
-        g.setColor(Color.white);
 
         int pixelsReference = Math.min(width, height);
 
@@ -23,10 +22,30 @@ public class GalaxyGraphicRepresentationUtils {
     }
 
     private static void paintStar(Graphics g, Star star, double initialMaxDistance, int pixelsReference) {
+
         Vector position = star.position();
+        double z = position.z();
+        int scaledColor = scaleColorZ(initialMaxDistance/5, z);
+
+        Color color = new Color(255, scaledColor, scaledColor);
+        g.setColor(color);
+
         int x = scaleToScreen(initialMaxDistance, pixelsReference, position.x());
         int y = scaleToScreen(initialMaxDistance, pixelsReference, position.y());
         g.fillOval(x, y, 3, 3);
+    }
+
+    private static int scaleColorZ(double distanceToScale, double z) {
+
+        int color = (int) (126 * z/distanceToScale + 128);
+
+        if (color < 0) {
+            return 0;
+        } else if (color > 255) {
+            return 255;
+        } else {
+            return color;
+        }
     }
 
     private static int scaleToScreen(double initialMaxDistance, int pixelsReference, double coordinate) {
