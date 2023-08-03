801864ce6b081e9fa87c61cbb8e3a3dd96cb6ba4
CiudadAlex <ciudad.alex@gmail.com>
2023-08-03 14:51:34 +0200

    initial commit

--- src/main/java/org/leviatanplatform/simulation/galaxy/Main.java
+++ src/main/java/org/leviatanplatform/simulation/galaxy/Main.java
@@ -17,7 +17,7 @@ public class Main {
         int numberOfStars = 1000;
         int millisToRepaint = 10;
         boolean relativistic = false;
-        GalaxyType galaxyType = GalaxyType.FLAT_ROTATORY;
+        GalaxyType galaxyType = GalaxyType.NO_RESTRICTION;
         boolean hasBlackHole = false;
         double velocityFactor = 1e6;
         double secondsJump = 3 * RandomGalaxyGenerator.MONTH;
